a696c6a7c43ff76ba4e9fbf039c2a9e12a251493
Andrea-Dispe <dispe.andrea@gmail.com>
2023-04-21 23:20:07 +0200

    made some UI fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,13 @@ import Contact from './components/Contact'
 import Banner from './components/Banner'
 import config from './config'
 import { getUserLanguage } from './utils/utils.js'
+import translationIT from './assets/translations-it.json'
+import translationEN from './assets/translations-en.json'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const addImagesThumbs = (projects) => {
-  return projects.map(project => {
-
-    return project
-  })
-
-}
-
 function App() {
   const { api } = config
-  console.log('config in client: ', config);
-  console.log('api in client: ', api);
-
   const [lang, setLang] = useState(getUserLanguage())
   const [projects, setProjects] = useState([])
 
@@ -29,19 +20,26 @@ function App() {
     document.querySelector('html').lang = lang
     axios.get(`${api}/translations/get/${lang}`)
       .then(result => {
-        const projects = addImagesThumbs(result.data.translations)
+        const projects = result.data.translations
         setProjects(projects)
+      })
+      .catch(error => {
+        console.error('Error while getting data from api', error)
+        // fallback in case the api fails
+        if (lang === 'en') {
+          setProjects(translationEN.translations)
+        } else if (lang === 'it') {
+          setProjects(translationIT.translations)
+        }
       })
   }, [lang])
 
-
-
   return (
     <div className="App">
-      <NavBar setLang={setLang} />
+      <NavBar setLang={setLang} lang={lang} />
       <Banner />
       <Projects projects={projects} />
-      <Contact lang={lang}/>
+      <Contact lang={lang} />
     </div>
   );
 }
